0b84ec6e7c537bceba14163f465bed61f17a0172
Kevin Morales <kfmorales94@gmail.com>
2022-01-23 20:23:04 -0500

    Day 001 done

--- Day 001/Day001.playground/Contents.swift
+++ Day 001/Day001.playground/Contents.swift
@@ -1,6 +1,42 @@
 import UIKit
 
-var greeting = "Hello, playground"
+var greeting = "Day 001"
 
-// VARIABLES
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+print(favoriteShow)
 
+var meaningOfLife = 42
+//meaningOfLife = "Forty two"
+
+var quote = "Change the world by being yourself"
+var burns = """
+The best laid schemes
+O’ mice and men
+Gang aft agley
+"""
+print(burns)
+
+var myInt = 1
+var myDouble = 1.0
+//let total = myInt + myDouble
+
+var city = "Cardiff"
+var message = "Welcome to \(city)!"
+print(message)
+
+let percentageDouble: Double = 99
+let percentageInt = 99
+
+// MARK: SETS are unordered and cannot contain duplicates, whereas ARRAYS retain their order and can contain duplicates.
+let arrayOfBlogCategories: [String] = ["Swift", "Debugging", "Xcode", "Workflow", "Optimization"]
+var setOfBlogCategories: Set<String> = ["Swift", "Swift", "Xcode", "Swift", "Optimization"]
+print(arrayOfBlogCategories)
+print(setOfBlogCategories)
+let (inserted, memberAfterInsert) = setOfBlogCategories.insert("Dart")
+if !inserted {
+    print("\(memberAfterInsert) already exists")
+} else {
+    print("\(memberAfterInsert) doesn't exists")
+}
